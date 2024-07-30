c29e86a498df7963dc1dd836bf68aca672762071
DENIS STORM <MAIL@MAIL>
2024-07-31 01:26:53 +0300

    ADD SEARCH BY FREQ

--- resources/views/_lvz/record-index.blade.php
+++ resources/views/_lvz/record-index.blade.php
@@ -10,6 +10,15 @@
             <a href="{{ route('app.import')}}" class="listA">
                 Получить записи
             </a>
+            <form action="{{ route('app.record.search') }}" method="post" class="listForm" id="form">
+                @csrf
+                <select class="listFormInput" id="input" name="recordSearchFreq">
+                    <option value="">Поиск по частоте</option>
+                    @foreach($freqs as $freq)
+                        <option value="{{ $freq->freq }}">{{ $freq->freq . ' --> ' . $freq->name }}</option>
+                    @endforeach
+                </select>
+            </form>
             <div class="listWrap">
                 @foreach($records as $record)
                     <div class="listWrapItem">
@@ -22,4 +31,9 @@
             </div>
         </div>
     </section>
+    <script>
+        document.getElementById('input').addEventListener('change', function() {
+            document.getElementById('form').submit();
+        });
+    </script>
 </x-l-layout::main>
